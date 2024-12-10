37e5ebc3ae221962dc45ff4cc7741aca3e1ffde2
NerOunt <kravlad568@gmail.com>
2024-12-10 22:12:41 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,24 @@
+
+$(document).ready(function () {
+	$('.multiple-items').slick({
+		infinite: true,
+		slidesToShow: 3,
+		slidesToScroll: 3,
+		dots: true,
+		responsive: [
+			{
+				breakpoint: 768,
+				settings: {
+					slidesToShow: 1,
+					slidesToScroll: 1,
+					centerMode: true,
+					centerPadding: '60px',
+				},
+			},
+		],
+	})
+})
+$('.slider').slick({
+    slidesToShow: 1, 
+    slidesToScroll: 1
+});
